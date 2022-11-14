8d0324767192b88fd8fdfa0703a9808f42273704
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-13 22:23:01 -0500

    Add files via upload

---
+++ packages/hoppscotch-app/dist/assets/Shortcode.3a518ed3.js
@@ -0,0 +1,2 @@
+import{ef as t,eg as o,eh as r}from"./index.8036b442.js";const s=e=>t(o,{request:JSON.stringify(e)}),a=e=>t(r,{code:e});export{s as c,a as d};
+//# sourceMappingURL=Shortcode.3a518ed3.js.map
