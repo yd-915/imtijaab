0064826d0a57855cd9b65ce0eeeee1138051640f
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-25 21:29:36 -0500

    Add files via upload

---
+++ packages/hoppscotch-app/src/components/app/GitHubStarButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <GithubButton
+    title="Forked from this magnificient project!"
+    href="https://github.com/hoppscotch/hoppscotch"
+    :data-color-scheme="
+      colorMode.value != 'light'
+        ? colorMode.value == 'black'
+          ? 'dark'
+          : 'dark_dimmed'
+        : 'light'
+    "
+    data-show-count="true"
+    data-text="OG Code"
+    aria-label="Forked from this magnificient project!"
+    :data-size="size"
+  />
+</template>
+
+<script setup lang="ts">
+import GithubButton from "vue-github-button"
+import { useColorMode } from "@composables/theming"
+
+const colorMode = useColorMode()
+
+defineProps({
+  size: {
+    type: String,
+    default: undefined,
+  },
+})
+</script>
