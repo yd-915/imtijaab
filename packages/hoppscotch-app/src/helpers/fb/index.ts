fedd3838883d5feb60c02864da078e33bc81fba4
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-26 20:50:38 -0500

    Update index.ts

--- packages/hoppscotch-app/src/helpers/fb/index.ts
+++ packages/hoppscotch-app/src/helpers/fb/index.ts
@@ -7,14 +7,13 @@ import { initHistory } from "./history"
 import { initSettings } from "./settings"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCE08vDZRnneJBWkTJGlIRK4osfiAHU4FM",
-  authDomain: "market-eed24.firebaseapp.com",
-  databaseURL: "https://market-eed24-default-rtdb.firebaseio.com",
-  projectId: "market-eed24",
-  storageBucket: "market-eed24.appspot.com",
-  messagingSenderId: "149290205276",
-  appId: "1:149290205276:web:9fd1db67dfa01722b4a699",
-  measurementId: "G-YT9VD9EE03",
+ apiKey: "AIzaSyApN5iYz4JhRyJHzEmTXleSE5yghZOvM3k",
+  authDomain: "saxichat.firebaseapp.com",
+  projectId: "saxichat",
+  storageBucket: "saxichat.appspot.com",
+  messagingSenderId: "336911539806",
+  appId: "1:336911539806:web:89eab0783d20c79587decf",
+  measurementId: "G-39G4LV1L7H"
 }
 
 let initialized = false
